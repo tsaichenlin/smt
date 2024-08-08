2d6a8f05e02b36cac11ce312e564b9c7efe16288
JaredMaeyama <maeyama.jared@gmail.com>
2024-08-08 03:18:39 -0700

    updated model

--- backend/server.py
+++ backend/server.py
@@ -1,18 +1,22 @@
 from flask import Flask, request, jsonify
 import tensorflow as tf
+from tensorflow.keras import regularizers
 import json
 import numpy as np
 from flask_cors import CORS
 
 class WeightedAverageLayer(tf.keras.layers.Layer):
-    def __init__(self, **kwargs):
+    def __init__(self, kernel_regularizer='l1', **kwargs):
         super(WeightedAverageLayer, self).__init__(**kwargs)
+        self.kernel_regularizer = regularizers.get(kernel_regularizer)
 
     def build(self, input_shape):
         # Initialize the trainable weights for the weighted average
         self.kernel = self.add_weight(shape=(input_shape[1],),
                                       initializer='random_normal',
-                                      trainable=True)
+                                      trainable=True,
+                                      regularizer=self.kernel_regularizer)
+
         # A small value added to weights to avoid division by zero
         self.epsilon = 1e-10
 
@@ -31,7 +35,6 @@ class WeightedAverageLayer(tf.keras.layers.Layer):
         # Calculate the weighted average
         weighted_average = weighted_sum / weights_sum
         return weighted_average
-
 app = Flask(__name__)
 CORS(app)
 tf.keras.config.enable_unsafe_deserialization()
@@ -39,7 +42,7 @@ print("start")
 batted_bal = json.load(open('./data/batted_ball_Input.json'))
 situation = json.load(open('./data/situation_Input.json'))
 print('json loaded')
-model = tf.keras.models.load_model('./model/saved_model_SitEmb_input0_refined (2).keras', custom_objects={'WeightedAverageLayer': WeightedAverageLayer})
+model = tf.keras.models.load_model('./model/saved_model_SitEmb_input0_refined_properWeighted_noPlayerKey_withAttention_fixedlineup_retrian_normalize.keras', custom_objects={'WeightedAverageLayer': WeightedAverageLayer})
 lineup = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 lineup = np.tile(np.array(lineup), (3890, 1))
 
@@ -52,6 +55,7 @@ for x in situation:
 base_RV = model.predict([
     np.asarray(batted_bal[:3890]).astype('float32'),
     np.asarray(situation_id[:3890]).astype('float32'),
+    np.asarray(lineup[:3890]).astype('float32'),
     np.asarray(lineup[:3890]).astype('float32')
 ])[0].sum()
 print('initialized')
@@ -72,12 +76,13 @@ def members():
         RV = model.predict([
             np.asarray(batted_bal[:3890]).astype('float32'),
             np.asarray(situation_id[:3890]).astype('float32'),
-            np.asarray(lineup[:3890]).astype('float32')
+            np.asarray(lineup[:3890]).astype('float32'),
+            np.asarray(np.tile(np.array([1,2,3,4,4,5,6,7,8,9]), (3890, 1))).astype('float32')
         ])[0].sum()
         if a==0:
             RV_lineup.append(float(base_RV - RV))
         else:
-             RV_lineup.append(float(RV_lineup[0] - RV + base_RV))
+             RV_lineup.append(float(base_RV - RV_lineup[0] - RV))
     ans['RV_player'] = RV_lineup[1:]
     ans['RV_lineup'] = RV_lineup[0]
     print(global_data['data'])  # Access the global variable
