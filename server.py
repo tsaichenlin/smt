5dc4755da69bf0f96f696f98111f1d790446bd67
Mandy Lin <91178384+tsaichenlin@users.noreply.github.com>
2024-08-09 00:42:26 -0700

    Update requirements

--- server.py
+++ server.py
@@ -115,4 +115,4 @@ def receive_data():
     return jsonify({'message': 'Data received successfully'}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0",port="5000",debug=True)
